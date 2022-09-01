bde7a1d257a4fc50de1e1dd59d6f00fb76a82978
Silver0006 <52266188+Silver0006@users.noreply.github.com>
2022-08-31 23:10:11 -0400

    Add files via upload

--- CottonCandy - 0.1.5.ps1
+++ CottonCandy - 0.1.5.ps1
@@ -1,13 +1,35 @@
-﻿function Show-Menu {
-    param (
-        [string]$Title = 'CottonCandy PS Designed by Aidan Patrick Marias'
-    )
+﻿#I hate Powershell 
+$start = function Show-Menu {
+    
+        $Title = 'CottonCandy 0.1.5 Designed by Aidan Patrick Marias'
+   
     Clear-Host
     Write-Host "================ $Title ================"
+    
+    Write-Host "1. Tools"
+    Write-Host "2: Install"
+    Write-Host "3: Press '3' for this option."
+    Write-Host "Q: Press 'Q' to quit."
+}
+do
+ {
+    Show-Menu
+    $selection = Read-Host "Please make a selection"
+    switch ($selection)
+#Tools Menu
+    {
+    '1' {
+    $tools = function Show-Menu {
+    
+        $Subtitle1 = 'Tools'
+   
+    Clear-Host
+    Write-Host "================ $Subtitle1 ================"
     
     Write-Host "1. Show Disabled Features"
     Write-Host "2: Enable Basic Admin Tools"
     Write-Host "3: Press '3' for this option."
+    Write-Host "B: Press 'B' to Return to Menu."
     Write-Host "Q: Press 'Q' to quit."
 }
 do
@@ -21,6 +43,51 @@ do
     } '2' {
     Enable-WindowsOptionalFeature –FeatureName "Windows-Defender-ApplicationGuard" -All -Online
     Enable-WindowsOptionalFeature –FeatureName "Name" -All -Online 
+    } '3' {
+      'You chose option #3'
+    } 'b' {
+    if ($selection = 'b') {return $start}
+    }
+    }
+    pause
+ }
+ until ($selection -eq 'q')
+
+ #Install Menu
+    } '2' {
+    $install = function Show-Menu {
+    
+        $Subtitle2 = 'Install'
+   
+    Clear-Host
+    Write-Host "================ $Subtitle2 ================"
+    
+    Write-Host "1. Install Chocolatey"
+    Write-Host "2: Update Chocolatey"
+    Write-Host "3: Uninstall Chocolatey"
+    Write-Host "B: Press 'B' to Return to Menu."
+    Write-Host "Q: Press 'Q' to quit."
+}
+do
+ {
+    Show-Menu
+    $selection = Read-Host "Please make a selection"
+    switch ($selection)
+    {
+    '1' {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; `
+  iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    } '2' {
+    choco upgrade chocolatey -y
+    } '3' {
+    choco uninstall chocolatey
+    } 'b' {
+    if ($selection = 'b') {return $start}
+    }
+    }
+    pause
+ }
+ until ($selection -eq 'q')
     } '3' {
       'You chose option #3'
     }
