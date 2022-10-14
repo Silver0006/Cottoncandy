f6772f26ae5f43bdcaf9690930c2173dd39a44c5
Aidan Patrick Marias <52266188+Silver0006@users.noreply.github.com>
2022-10-14 00:06:14 -0400

    Add files via upload

--- CottonCandy - 0.1.5.ps1
+++ CottonCandy - 0.1.5.ps1
@@ -1,5 +1,5 @@
 ﻿#I hate Powershell 
-set-executionpolicy remotesigned
+set-executionpolicy Unrestricted
 $start = function Show-Menu {
     
         $Title = 'CottonCandy 0.1.5 Designed by Aidan Patrick Marias'
@@ -30,6 +30,7 @@ do
     Write-Host "1. Show Disabled Features"
     Write-Host "2: Enable Powershell Scripts"
     Write-Host "3: FETCH"
+    Write-Host "3: Final Flash"
     Write-Host "B: Press 'B' to Return to Menu."
     Write-Host "Q: Press 'Q' to quit."
 }
@@ -51,6 +52,12 @@ do
     } '3' {
     #Used to allow disable restrictions for script 
       'You chose option #3'
+    } '4' {
+    
+    
+    
+    
+    set-executionpolicy AllSigned
     } 'B' {
     if ($selection -eq 'B') {return $start}
     else {return $tools}
@@ -76,6 +83,9 @@ do
     Write-Host "5: Install Firefox"
     Write-Host "6: Install Notepad++"
     Write-Host "7: Install Malwarebytes" 
+    Write-Host "8: Install 7Zip" 
+    Write-Host "9: Install Google Chrome" 
+    Write-Host "10: Install MyEventViewer" 
     Write-Host "B: Press B to Return to Menu."
     Write-Host "Q: Press Q to quit."
 }
@@ -102,6 +112,10 @@ do
     choco install malwarebytes -y
     } "8" {
     choco install 7zip -y
+    } "9" {
+    choco install googlechrome -y
+    } "10" {
+    choco install myeventviewer -y
     } 'b' {
     if ($selection = 'b') {return $start}
     }
@@ -119,11 +133,11 @@ do
     
     Write-Host "1. Disable Guest and Admin Accounts"
     Write-Host "2: Rename Guest and Admin Accounts"
-    Write-Host "3: Resets Windows Firewall to Default Settings"
-    Write-Host "4: Block Microsoft Account Login"
-    Write-Host "5: Enable Ctrl+Alt+Del to logon"
+    Write-Host "3: Enables Windows Firewall & Defender"
+    Write-Host "4: Block Microsoft Account Login & Onedrive"
+    Write-Host "5: Login Settings"
     Write-Host "6: Turns on Automatic Updates"
-    Write-Host "7: "
+    Write-Host "7: Enable Smart Screen"
     Write-Host "B: Press 'B' to Return to Menu."
     Write-Host "Q: Press 'Q' to quit."
 }
@@ -140,22 +154,80 @@ do
     Rename-LocalUser -Name "guest" -NewName "Holo"
     } "3" {
     (New-Object -ComObject HNetCfg.FwPolicy2).RestoreLocalFirewallDefaults()
+    New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows Defender Security Center"
+    New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows Defender" 
+    New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Value ”0”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender" -Name "DisableAntiSyware" -Value ”0” 
+    New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows Defender\Real-Time Protection"
+    New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" -Name "DisableIOAVProtection" -Value ”0”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" -Name "DisableIOAVProtection" -Value ”0” 
+    New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" -Name "DisableRealtimeMonitoring" -Value ”0”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" -Name "DisableRealtimeMonitoring" -Value ”0” 
+    New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows\Installer"
+    New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Installer" -Name "AlwaysInstallElevated" -Value ”0”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Installer" -Name "AlwaysInstallElevated" -Value ”0” 
+    New-Item -Path "HKCU:\Software\Policies\Microsoft\Windows\Installer"
+    New-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Installer" -Name "AlwaysInstallElevated" -Value ”0”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\Windows\Installer" -Name "AlwaysInstallElevated" -Value ”0” 
+    New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Installer" -Name "AlwaysInstallElevated" -Value ”0”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\Installer" -Name "AlwaysInstallElevated" -Value ”0"
+    New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows Defender Security Center\Virus and threat protection" 
+    New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender Security Center\Virus and threat protection" -Name "UILockdown" -Value ”0”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender Security Center\Virus and threat protection" -Name "UILockdown" -Value ”0” 
+    New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows Defender Security Center\App and Browser protection"
+    New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender Security Center\App and Browser protection" -Name "DisallowExploitProtectionOverride" -Value ”1”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender Security Center\App and Browser protection" -Name "DisallowExploitProtectionOverride" -Value ”1” 
+    New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender Security Center\App and Browser protection" -Name "UILockdown" -Value ”0”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender Security Center\App and Browser protection" -Name "UILockdown" -Value ”0” 
+    New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender Security Center\Virus and threat protection" -Name "HideRansomwareRecovery" -Value ”0”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender Security Center\Virus and threat protection" -Name "HideRansomwareRecovery" -Value ”0” 
+    New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows Defender Security Center\Device performance and health"
+    New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender Security Center\Device performance and health" -Name "UILockdown" -Value ”0”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender Security Center\Device performance and health" -Name "UILockdown" -Value ”0” 
+    New-Item -Path "HKLM:\Software\Policies\Microsoft\Windows Defender Security Center\Notifications"
+    New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender Security Center\Notifications" -Name "DisableEnhancedNotifications" -Value ”0”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows Defender Security Center\Notifications" -Name "DisableEnhancedNotifications" -Value ”0” 
+
+
+   ## \SOFTWARE\Policies\Microsoft\Windows Defender Security Center\App and Browser protection
+    
+
     } "4" {
     New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "NoConnectedUser" -Value ”3”  -PropertyType "DWord"
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "NoConnectedUser" -Value ”3” 
+    New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive"
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive" -Name "DisableFileSyncNGSC" -Value ”1”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive" -Name "DisableFileSyncNGSC" -Value ”1” 
     } "5" {
-    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableCAD" -Value ”0”  
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableCAD" -Value ”0”  -PropertyType "DWord"
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableCAD" -Value ”0”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DisableCAD" -Value ”0”  
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DontDisplayLastUserName" -Value ”1”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "DontDisplayLastUserName" -Value ”1” 
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "InactivityTimeoutSecs" -Value ”900”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "InactivityTimeoutSecs" -Value ”900” 
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ShutdownWithoutLogon" -Value ”0”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ShutdownWithoutLogon" -Value ”0” 
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value ”1”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Value ”1” 
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorUser" -Value ”0”  -PropertyType "DWord"
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorUser" -Value ”0” 
+     
     } "6" {
-    #Using for powershell regkey testing
-    New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "AUOptions" -Value ”3”  -PropertyType "DWord"
-    Set-Itemproperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "AUOptions" -Value ”3” 
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "AUOptions" -Value ”4”  -PropertyType "DWord"
+    Set-Itemproperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "AUOptions" -Value ”4” 
     New-Itemproperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "NoAutoUpdate" -Value ”0”  -PropertyType "DWord"
     Set-Itemproperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "NoAutoUpdate" -Value ”0” 
-    New-Itemproperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "SetDisableUXWUAccess" -Value ”0”  -PropertyType "DWord"
-    Set-Itemproperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "SetDisableUXWUAccess" -Value ”0” 
+    New-Itemproperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name "SetDisableUXWUAccess" -Value ”0”  -PropertyType "DWord"
+    Set-Itemproperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name "SetDisableUXWUAccess" -Value ”0” 
     New-Itemproperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name "SetupdateNotificationLevel" -Value ”0”  -PropertyType "DWord"
     Set-Itemproperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name "SetupdateNotificationLevel" -Value ”0” 
+    New-Itemproperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name "SetupdateNotificationLevel" -Value ”0”  -PropertyType "DWord"
+    Set-Itemproperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name "SetupdateNotificationLevel" -Value ”0” 
+    } "7" {
+    New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "EnableSmartScreen" -Value ”2”  -PropertyType "DWord"
+    Set-Itemproperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "EnableSmartScreen" -Value ”2” 
+
+
     }
     }
     pause
