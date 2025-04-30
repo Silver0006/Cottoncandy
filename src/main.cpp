a488f2cac40c20a6501e761f23d773841853ddf9
Silver0006 <52266188+Silver0006@users.noreply.github.com>
2025-04-30 15:26:54 -0400

    Wrote WindowsSystemSettings::CheckPermLevel()

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,25 @@ int main() {
     WindowsSystemSettings current;
 
     std::cout << "This is a test" << std::endl;
-    current.CheckPermLevel();
+    //Checks if running with Elevated Privileges
+    if (current.CheckPermLevel()) {
+        std::cout << "This Program is running with Elevated Privileges" << std::endl;
+    } else {
+        std::cout << "This Program is not running with Elevated Privileges. Some features may be limited." 
+        "\nIt is recommened you run as administrator for the full experience." << std::endl;
+    }
+    //
+    current.SettingExists();
     
+
+
+    //delete below when done with testing
+    char UserWantsToExit = 0;
+    while (UserWantsToExit != 'q' && UserWantsToExit != 'Q')
+    {
+        std::cout << "\n\ntype q or Q to exit program: ";
+        std::cin >> UserWantsToExit;
+    }
+    return 0;
+
 }
--- src/windows/win32_interface.cpp
+++ src/windows/win32_interface.cpp
@@ -1,12 +1,32 @@
 #ifdef _WIN32
-#include "../../include/OS_interface.hpp"
 #include "../../include/windows/win32_interface.hpp"
+#include "../../include/OS_interface.hpp"
+#include <windows.h>
+/*NOTE: windows.h was designed for C meaning most of the memory allocation needs to be done manually
+or has features that are far more useful in c than c++*/
 
-
-
+//Pulls Cottoncandy's elevation status by checking current processes access token.
+//Uses OpenProcessToken() & GetTokenInformation() from windows.h
 bool WindowsSystemSettings::CheckPermLevel() {
-    std::cout << "Windows check perm test" << std::endl;
-    return true;
+    bool IsElevated = false;
+    HANDLE token = nullptr;
+    //Windows uses access tokens to display permisions of user. 
+    //Each process is given a copy of the token which serves as a way to identify program permisions and the user who activated said program.
+    //The below code is calling the current process and requesting it's token, which it stores in the token HANDLE that was declared above.
+    //TOKEN_QUERY serves as the level of access to the token. This is using memory so it need to be closed.
+    if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &token)){
+        TOKEN_ELEVATION elevationinfo;
+        DWORD StructSize;
+        //TokenElevation is the info being requested. elevationinfo is the output for a TOKEN_ELEVATION struct.
+        //sizeof(elevationinfo) is the size allocated for elevationinfo.
+        //StructSize is provided with the size of the TOKEN_ELEVATION struct for debugging.
+        if (GetTokenInformation(token, TokenElevation, &elevationinfo, sizeof(elevationinfo), &StructSize)) {
+            IsElevated = elevationinfo.TokenIsElevated;
+        }
+    }
+    //closing the token, cause it was loaded into memory when opened.
+    if (token) CloseHandle(token);
+    return IsElevated;
 }
 bool WindowsSystemSettings::SettingExists() {
     std::cout << "Windows check settings exist" << std::endl;
